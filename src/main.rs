1e4ce6383aeab1a4791da19f250db334660c0bee
Alexander Battisti <battisti@gmail.com>
2014-10-16 18:48:28 +0200

    updated to current Nickel version

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 extern crate nickel;
 
 use std::io::net::ip::Ipv4Addr;
-use nickel::{ Nickel, Request, Response };
+use nickel::{ Nickel, Request, Response, HttpRouter };
 
 fn main() {
     let mut server = Nickel::new();
+    let mut router = Nickel::router();
     
     fn a_handler (_request: &Request, response: &mut Response) { 
-            response.send("What the...a Rust-powered web app?"); 
-        }
+        response.send("What the...a Rust-powered web app?"); 
+    }
 
-    server.get("/", a_handler);
+    router.get("/", a_handler);
+    server.utilize(router);
     server.listen(Ipv4Addr(0, 0, 0, 0), 8080);
 }
